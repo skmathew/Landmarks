9d14624986a0366ca7514027f92139d3c6002e2e
Sherin Mathew <skm20011@gmail.com>
2023-08-19 12:55:09 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sherin Mathew on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
