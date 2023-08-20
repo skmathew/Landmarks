e0c8d9bbbb4bb72a01b1c2df58a23c25e68a80b0
Sherin Mathew <skm20011@gmail.com>
2023-08-20 11:45:11 -0500

    Added ima UI

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "bolt")
-                .imageScale(.large)
-                .font(.system(size: 100))
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
